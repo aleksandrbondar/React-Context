cd1d6624d6464d58f9e93ce949d12d1964ebf756
aleksandrbondar <aleksandr_bondar@icloud.com>
2024-06-30 21:10:13 +0300

    v2

--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -1,37 +1,31 @@
+import { useContext } from 'react'
 import { AppRadio } from './App/App'
 
 const InputGroup = () => {
+  const { setMethod } = useContext(AppRadio);
   return (
-    <AppRadio.Consumer>
-      {(value) => {
-        const { setMethod } = value
-        return (
-          <form className='inputGroup'>
-            <div>
-              <input
-                type='radio'
-                value="PropDrilling"
-                name='method'
-                onChange={(e) => setMethod(e.target.value)}
-                defaultChecked={true}
-              />
-              <label>Prop Drilling</label>
-            </div>
+    <form className='inputGroup'>
+      <div>
+        <input
+          type='radio'
+          value="PropDrilling"
+          name='method'
+          onChange={(e) => setMethod(e.target.value)}
+          defaultChecked={true}
+        />
+        <label>Prop Drilling</label>
+      </div>
 
-            <div>
-              <input
-                type='radio'
-                value="WithContext"
-                name='method'
-                onChange={(e) => setMethod(e.target.value)}
-              />
-              <label>Use Context</label>
-            </div>
-          </form>
-        )
-
-      }}
-    </AppRadio.Consumer>
+      <div>
+        <input
+          type='radio'
+          value="WithContext"
+          name='method'
+          onChange={(e) => setMethod(e.target.value)}
+        />
+        <label>Use Context</label>
+      </div>
+    </form>
   )
 }
 
